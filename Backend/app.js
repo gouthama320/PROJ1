dde0ae889424dc7d5ec3fc0a3e0c71d800cd2270
gouthama320 <83681220+gouthama320@users.noreply.github.com>
2025-10-08 17:36:11 -0400

    Add files via upload

---
+++ Backend/app.js
@@ -0,0 +1,125 @@
+// Backend: application services, accessible by URLs
+
+
+const express = require('express')
+const cors = require ('cors')
+const dotenv = require('dotenv')
+const path = require('path');
+dotenv.config()
+
+const app = express();
+
+const dbService = require('./dbService');
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+// Serve the frontend by manually defining the Frontend folder path
+app.use(express.static(path.join(__dirname, '../Frontend')));
+
+// create
+app.post('/insert', (request, response) => {
+    console.log("app: insert a row.");
+    // console.log(request.body); 
+
+    const {name} = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+ 
+    // note that result is a promise
+    result 
+    .then(data => response.json({data: data})) // return the newly added row to frontend, which will show it
+   // .then(data => console.log({data: data})) // debug first before return by response
+   .catch(err => console.log(err));
+});
+
+// read 
+app.get('/getAll', (request, response) => {
+    
+    const db = dbService.getDbServiceInstance();
+
+    
+    const result =  db.getAllData(); // call a DB function
+
+    result
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+
+app.get('/search/:name', (request, response) => { // we can debug by URL
+    
+    const {name} = request.params;
+    
+    console.log(name);
+
+    const db = dbService.getDbServiceInstance();
+
+    let result;
+    if(name === "all") // in case we want to search all
+       result = db.getAllData()
+    else 
+       result =  db.searchByName(name); // call a DB function
+
+    result
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
+});
+
+
+// update
+app.patch('/update', 
+     (request, response) => {
+          console.log("app: update is called");
+          //console.log(request.body);
+          const{id, name} = request.body;
+          console.log(id);
+          console.log(name);
+          const db = dbService.getDbServiceInstance();
+
+          const result = db.updateNameById(id, name);
+
+          result.then(data => response.json({success: true}))
+          .catch(err => console.log(err)); 
+
+     }
+);
+
+// delete service
+app.delete('/delete/:id', 
+     (request, response) => {     
+        const {id} = request.params;
+        console.log("delete");
+        console.log(id);
+        const db = dbService.getDbServiceInstance();
+
+        const result = db.deleteRowById(id);
+
+        result.then(data => response.json({success: true}))
+        .catch(err => console.log(err));
+     }
+)   
+
+app.post('/addUser', (request, response) => {
+    console.log("app: addUser called.");
+    const { username, password } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewUser(username, password);
+
+    result
+      .then(data => response.json({ success: true, data }))
+      .catch(err => {
+          console.error(err);
+          response.status(500).json({ success: false, error: err.message });
+      });
+});
+
+// if we configure here directly
+app.listen(5050, 
+    () => {
+        console.log("I am listening on the fixed port 5050.")
+    }
+);
